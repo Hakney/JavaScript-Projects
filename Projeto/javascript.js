842d3bcf4e1bcc9a3578197f405a1f6db33387e0
Hakney de Sousa Lima <hakney.lima@dnocs2.local>
2019-08-27 16:51:31 -0300

    Otimizando código

--- Projeto/javascript.js
+++ Projeto/javascript.js
@@ -1,23 +1,20 @@
 // Botão CHECAR
-$('#checar_idade').click(function (){
-    let data = new Date()
-        ano = data.getFullYear()
+$('#checar_idade').click(function checarIdade(){
+    let data = new Date(),
+        ano = data.getFullYear() 
         fano = document.getElementById('idade')
         res = document.querySelector('div#res')
-        erroAlert = alert('[ERRO] Verifique os dados novamente!')
-
-        if(fano.value.length == 0 || Number(fano.value) > ano){
-            return erroAlert
+    if(fano.value.length == 0 || Number(fano.value) > ano){
+        alert('[ERRO] Verifique os dados novamente!')
     }  else {
         let fsex = document.getElementsByName('radsex'),
             idade = ano - Number(fano.value),
             genero = ''
-            // Checa o gênero 
-            fsex[0].checked ? genero = 'Homem' :
-            fsex[1].checked ? genero = 'Mulher' :
+            // check gênero
+            fsex[0].checked ? genero = 'Homem' : fsex[1].checked ? genero = 'Mulher' : ''
 
+        $('#res').html(`${genero} de ${idade} anos`)
     }
-    $('#res').html(`${genero} de ${idade} anos`)
 })
 
 
