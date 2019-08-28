749835d0e5d609a435c810b1c0e86a78361c807b
Hakney de Sousa Lima <hakney.lima@dnocs2.local>
2019-08-28 15:33:26 -0300

    Verificador de idade - 2019

--- Projeto/javascript.js
+++ Projeto/javascript.js
@@ -3,10 +3,9 @@ $('#checar_idade').click(function checarIdade(){
     let data = new Date(),
         anoAtual = data.getFullYear(), 
         Nascimento = $('#idade').val();
-
-    if(Nascimento == 0 || Nascimento > anoAtual){
+    if(Nascimento <= 0 || Nascimento > anoAtual){
         alert('[ERRO] Verifique os dados novamente!')        
-    }  else {
+    } else {
         let fsex = document.getElementsByName('radsex'),
             idade = anoAtual - Nascimento,
             genero = ''
