5bdb9df67186af31d1a2cf80778627d291e14dd0
Hakney de Sousa Lima <hakney.lima@dnocs2.local>
2019-09-09 16:11:02 -0300

    Otimização completa do sistema

--- Projeto/javascript.js
+++ Projeto/javascript.js
@@ -3,10 +3,10 @@ $('#checar_idade').click(function checarIdade(){
         anoAtual = data.getFullYear(), 
         Nascimento = $('#idade').val();
     if(Nascimento <= 0 || Nascimento > anoAtual){
-         alert('[ERRO] Verifique os dados novamente!')        
+        alert('[ERRO] Verifique os dados novamente!')        
     } else {
-            idade = anoAtual - Nascimento,
-            genero = $("input[name='radsex']:checked").val();
-            genero ? $('#res').html(`${genero} de ${idade} anos`) : alert('Gênero não foi selecionado!')   
+        idade = anoAtual - Nascimento,
+        genero = $("input[name='radsex']:checked").val();
+        genero ? $('#res').html(`${genero} de ${idade} anos`) : alert('Gênero não foi selecionado!')   
     }
 })
